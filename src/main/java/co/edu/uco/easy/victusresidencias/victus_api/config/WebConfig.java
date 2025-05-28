d40935c94521eb9882e4a21348f459cc5c9daee4
Andres Velez <andresjobpk@gmail.com>
2025-05-28 18:33:41 -0500

    Configuracion para aceptar los corns

--- src/main/java/co/edu/uco/easy/victusresidencias/victus_api/config/WebConfig.java
+++ src/main/java/co/edu/uco/easy/victusresidencias/victus_api/config/WebConfig.java
@@ -1,2 +1,22 @@
-package co.edu.uco.easy.victusresidencias.victus_api.config;public class WebConfig {
+package co.edu.uco.easy.victusresidencias.victus_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173") // Cambia esto si usas otro puerto
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
