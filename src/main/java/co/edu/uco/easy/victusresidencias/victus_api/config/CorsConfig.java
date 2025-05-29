167c87b5c24e0ddf0e8207320d4a0d32e6f3f295
Andres Velez <andresjobpk@gmail.com>
2025-05-28 21:48:20 -0500

    clase para usar configuracion global para Corns

--- src/main/java/co/edu/uco/easy/victusresidencias/victus_api/config/CorsConfig.java
+++ src/main/java/co/edu/uco/easy/victusresidencias/victus_api/config/CorsConfig.java
@@ -6,14 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Cambia esto si usas otro puerto
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://tangerine-profiterole-824fd8.netlify.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
